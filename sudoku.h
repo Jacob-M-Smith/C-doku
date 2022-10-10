55e2f8b0aa63c304230adc2ef2edbb42cb3408ee
Jacob M. Smith <jmsmith6101@att.net>
2022-10-10 17:57:42 -0400

    defines

--- sudoku.h
+++ sudoku.h
@@ -1,6 +1,16 @@
-#ifndef "SUDOKU"
-#define "SUDOKU"
+#ifndef SUDOKU
+#define SUDOKU
 
-void init_tracker(struct node**);
+#define ROW 1
+#define COL 2
+#define SQR 3
+
+// tracks the size of lls in trackers arrays
+int row_size[9] = {9, 9, 9, 9, 9, 9, 9, 9, 9};
+int col_size[9] = {9, 9, 9, 9, 9, 9, 9, 9, 9};
+int sqr_size[9] = {9, 9, 9, 9, 9, 9, 9, 9, 9};
+
+void init_tracker(node**);
+// void update_row(node**, int[9][9])
 
 #endif
