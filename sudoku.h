2af5cbfe0ffbacaf7abfe9b23aee1d337bea0efa
Jacob M. Smith <jmsmith6101@att.net>
2022-10-12 22:15:19 -0400

    changed init_tracker return type

--- sudoku.h
+++ sudoku.h
@@ -6,11 +6,11 @@
 #define SQR 3
 
 // tracks the size of lls in trackers arrays
-int row_size[9] = {9, 9, 9, 9, 9, 9, 9, 9, 9};
-int col_size[9] = {9, 9, 9, 9, 9, 9, 9, 9, 9};
-int sqr_size[9] = {9, 9, 9, 9, 9, 9, 9, 9, 9};
+// int row_size[9] = {9, 9, 9, 9, 9, 9, 9, 9, 9};
+// int col_size[9] = {9, 9, 9, 9, 9, 9, 9, 9, 9};
+// int sqr_size[9] = {9, 9, 9, 9, 9, 9, 9, 9, 9};
 
-void init_tracker(node**);
-void update_row(node**, int[9][9]);
+node* init_tracker();
+void update_row(node**);
 
 #endif
